29c48f14fec8f7555b6dee9ab83aa8d4c100c6a9
hanif <h.ehmedov@gmail.com>
2023-07-05 18:27:15 -0400

    before alfa-betta-prune

--- src/comps/utils/ai.js
+++ src/comps/utils/ai.js
@@ -1,90 +1,156 @@
 /* eslint-disable */
 import * as helper from "./helper";
 
-
-
 // INFO: minimax
-export const minimax = (small, board, positions, aiturn, sum, depth) => {
-    let emptyCells = helper.getAvailableCells(board);
+// export const minimax = (small, board, positions, aiturn, sum, depth) => {
 
-    // every possible move( ai or human) calculates 1
-    sum++;
+//     let emptyCells = helper.getAvailableCells(board);
+//     let win = helper.checkWinner(small, board, positions)
 
-    // edge cases
+//     // every possible move( ai or human) calculates 1
 
-    if (helper.checkWinner(small, board, positions) === 1) {
-        // if(depth == 3) return { val: 10, anaz: sum, depth };
-        return { val: -1, anaz: sum, depth };
-    } else if (helper.checkWinner(small, board, positions) === 0) {
-        return { val: 1, anaz: sum, depth };
-    } else if (emptyCells.length === 0) {
-        return { val: 0, anaz: sum, depth };
-    }
+//     sum++;
+//     // edge cases
 
-  
+//     // if player: x wins
+//     if (win === 1) {
+//         // if(depth == 3) return { val: 10, anaz: sum, depth };
+//         return { val: -1, anaz: sum, depth };
+//     } 
+    
+//     // if ai: o wins
+//     else if ( win === 0) return { val: 1, anaz: sum, depth };
 
-    if (aiturn) {
-        // ai turn
-        let bestScore = { val: -Infinity, anaz: 0, depth };
+//     // if draw
+//     else if (emptyCells.length === 0) return { val: 0, anaz: sum, depth };
 
-        for (let i = 0; i < board.length; i++) {
-            if (board[i] === "") {
-                board[i] = "O";
-                let score = minimax(
-                    small,
-                    board,
-                    positions,
-                    false,
-                    sum,
-                    depth + 1
-                );
-                board[i] = "";
+//     // stop after depth 7 
+//     else if (depth >= 7) return 0;
 
-                bestScore = {
-                    val: Math.max(score.val, bestScore.val),
-                    anaz: score.anaz + bestScore.anaz,
-                    depth,
-                };
-            }
-        }
 
-        return bestScore;
-    } else {
-        // human turn
-        let bestScore = { val: Infinity, anaz: 0, depth };
+//     if (aiturn) {
+//         // ai turn
+//         let bestScore = { val: -Infinity, anaz: 0, depth };
 
-        for (let i = 0; i < board.length; i++) {
-            if (board[i] === "") {
-                board[i] = "X";
-                let score = minimax(
-                    small,
-                    board,
-                    positions,
-                    true,
-                    sum,
-                    depth + 1
-                );
+//         for (let i of emptyCells) {
+//             board[i] = "O";
 
-                board[i] = "";
+//             // call minimax recursivle
+//             let score = minimax(small, board, positions, false, sum, depth + 1);
+//             board[i] = "";
 
-                bestScore = {
-                    val: Math.min(score.val, bestScore.val),
-                    anaz: score.anaz + bestScore.anaz,
-                    depth,
-                };
-            }
-        }
+//             // maximize bestscore for ai
+//             bestScore = {
+//                 val: Math.max(score.val, bestScore.val),
+//                 anaz: score.anaz + bestScore.anaz,
+//                 depth,
+//             };
+//         }
 
-        return bestScore;
-    }
+//         return bestScore;
+
+//     } else {
+//         // human turn
+//         let bestScore = { val: Infinity, anaz: 0, depth };
+
+//         for (let i of emptyCells) {
+//             board[i] = "X";
+//               // call minimax recursivle
+
+//             let score = minimax(small, board, positions, true, sum, depth + 1);
+
+//             board[i] = "";
+
+//             // minimize hu scores ( -10 is best spot than -1)
+//             bestScore = {
+//                 val: Math.min(score.val, bestScore.val),
+//                 anaz: score.anaz + bestScore.anaz,
+//                 depth,
+//             };
+
+
+//         }
+
+//         return bestScore;
+//     }
+// };
+
+
+
+
+
+
+export const minimax = (small, alfa, betta, prune,  board,  positions, aiturn, sum, depth) => {
+
+
+
+    // let emptyCells = helper.getAvailableCells(board);
+    // let win = helper.checkWinner(small, board, positions)
+
+    // // every possible move( ai or human) calculates 1
+
+    // sum++;
+    // // edge cases
+
+    // // if player: x wins
+    // if (win === 1) {
+    //     // if(depth == 3) return { val: 10, anaz: sum, depth };
+    //     return { val: -1, anaz: sum, depth };
+    // } 
+    
+    // // if ai: o wins
+    // else if ( win === 0) return { val: 1, anaz: sum, depth };
+
+    // // if draw
+    // else if (emptyCells.length === 0) return { val: 0, anaz: sum, depth };
+
+    // // stop after depth 7 
+    // else if (depth >= 7) return 0;
+
+
+    // if (aiturn) {
+    //     // ai turn
+    //     let bestScore = { val: -Infinity, anaz: 0, depth };
+
+    //     for (let i of emptyCells) {
+    //         board[i] = "O";
+
+    //         // call minimax recursivle
+    //         let score = minimax(small, board, positions, false, sum, depth + 1);
+    //         board[i] = "";
+
+    //         // maximize bestscore for ai
+    //         bestScore = {
+    //             val: Math.max(score.val, bestScore.val),
+    //             anaz: score.anaz + bestScore.anaz,
+    //             depth,
+    //         };
+    //     }
+
+    //     return bestScore;
+
+    // } else {
+    //     // human turn
+    //     let bestScore = { val: Infinity, anaz: 0, depth };
+
+    //     for (let i of emptyCells) {
+    //         board[i] = "X";
+    //           // call minimax recursivle
+
+    //         let score = minimax(small, board, positions, true, sum, depth + 1);
+
+    //         board[i] = "";
+
+    //         // minimize hu scores ( -10 is best spot than -1)
+    //         bestScore = {
+    //             val: Math.min(score.val, bestScore.val),
+    //             anaz: score.anaz + bestScore.anaz,
+    //             depth,
+    //         };
+
+            
+    //     }
+
+    //     return bestScore;
+    // }
 };
-
-/* INFO: Hope you are done 
-    in board position [ 'x', 'o', 'x', '', 'o', '', 'x', '' ,'' ]
-    minimax take index 3 and make itself 2 winning position and wins
-    but index 6 wins directly
-
-    TODO 
-    add depth value and return val/index with min depth to play the shorttest win index
- */
-
